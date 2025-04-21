b0dc9082af62012b869f5741b67cea95073ded39
Everton Jesus <evertonjesus8613@gmail.com>
2025-04-21 19:07:58 -0300

    estilização

--- cadastrar.php
+++ cadastrar.php
@@ -4,13 +4,89 @@
   <meta charset="UTF-8">
   <title>CRUD BY EVERTON</title>
 </head>
+<style>
+    body {
+      font-family: Arial, sans-serif;
+      background: #f0f2f5;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
+
+    .login-container {
+  background-color: rgba(207, 239, 248, 0.75);
+      padding: 70px;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      width: 300px;
+    }
+
+    .login-container h2 {
+      text-align: center;
+      margin-bottom: 20px;
+      color: #333;
+    }
+
+    .login-container input[type="text"],
+    .login-container input[type="password"] {
+      width: 80%;
+      padding: 20px;
+      margin: 5px 0;
+      border: 1px solid #ccc;
+      border-radius: 10px;
+    }
+
+    .login-container button {
+      width: 70%;
+      padding: 10px;
+      background-color: #4CAF50;
+      border: none;
+      color: white;
+      border-radius: 5px;
+      font-size: 16px;
+      cursor: pointer;
+    }
+
+    .login-container button {
+  width: 50%;
+  padding: 15px;
+  background-color: rgb(23, 192, 214);
+  border: none;
+  color: white;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  display: block;
+  margin: 20px auto 0;
+}
+
+
+    .login-container p {
+      text-align: center;
+      margin-top: 15px;
+      color: #666;
+    }
+
+    body {
+    font-family: Arial, sans-serif;
+    background: url('https://images3.alphacoders.com/132/thumb-1920-1322308.jpeg') no-repeat center center fixed;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+  }
+  </style>
 <body>
   <div class="login-container">
     <h2>Criar uma conta</h2>
     <form action="cliente.php" method="POST">
-        <input type="text" name="nome" class="inputUser" placeholder="nome" required><p>
-        <input type="text" name="email" class="inputUser" placeholder="email" required><p>
-        <input type="password" name="senha" class="inputUser" placeholder="Senha" required><p>
+        <input type="text" name="nome" class="inputUser" placeholder="nome" required>
+        <input type="text" name="email" class="inputUser" placeholder="email" required>
+        <input type="password" name="senha" class="inputUser" placeholder="Senha" required>
         <button type="submit" name="btn-cadastrar">Fazer cadastro</button><p>
         <a href="index.php">Voltar</a>
     </form>
--- index.php
+++ index.php
@@ -48,14 +48,93 @@
   <meta charset="UTF-8">
   <title>CRUD BY EVERTON</title>
 </head>
+
+
 <body>
+<style>
+    body {
+      font-family: Arial, sans-serif;
+      background: #f0f2f5;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
+
+    .login-container {
+  background-color: rgba(207, 239, 248, 0.75);
+      padding: 70px;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      width: 300px;
+    }
+
+    .login-container h2 {
+      text-align: center;
+      margin-bottom: 20px;
+      color: #333;
+    }
+
+    .login-container input[type="text"],
+    .login-container input[type="password"] {
+      width: 80%;
+      padding: 20px;
+      margin: 5px 0;
+      border: 1px solid #ccc;
+      border-radius: 10px;
+    }
+
+    .login-container button {
+      width: 70%;
+      padding: 10px;
+      background-color: #4CAF50;
+      border: none;
+      color: white;
+      border-radius: 5px;
+      font-size: 16px;
+      cursor: pointer;
+    }
+
+    .login-container button {
+  width: 50%;
+  padding: 15px;
+  background-color: rgb(23, 192, 214);
+  border: none;
+  color: white;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  display: block;
+  margin: 20px auto 0;
+}
+
+
+    .login-container p {
+      text-align: center;
+      margin-top: 15px;
+      color: #666;
+    }
+
+    body {
+    font-family: Arial, sans-serif;
+    background: url('https://images3.alphacoders.com/132/thumb-1920-1322308.jpeg') no-repeat center center fixed;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+  }
+  </style>
+  
   <div class="login-container">
     <h2>Acesse sua conta</h2>
     <form action="index.php" method="POST">
-      <input type="text" name="email" class="inputUser" placeholder="email" required><p>
-      <input type="password" name="senha" class="inputUser" placeholder="Senha" required><p>
-      <button type="submit" name="btn-entrar">Entrar</button>
-      <a href="cadastrar.php">Cadastre-se</a>
+      <input type="text" name="email" class="inputUser" placeholder="email" required>
+      <input type="password" name="senha" class="inputUser" placeholder="Senha" required>
+      <button type="submit" name="btn-entrar">Entrar</button><p>
+      <a href="cadastrar.php">Cadastre-se</a><p>
       
     </form>
   </div>
